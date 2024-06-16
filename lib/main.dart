9d1c6ba43e8d7452cb0a9e2d4ddfb6cc6513820a
unknown <ecar1234@gmail.com>
2024-06-16 20:34:07 +0900

    add follow page

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
+import 'package:app_the_clark/presentation/pages/follow_page.dart';
 import 'package:app_the_clark/presentation/pages/home.dart';
 import 'package:app_the_clark/presentation/pages/my_page.dart';
 import 'package:app_the_clark/presentation/pages/purchase_list.dart';
+
 import 'package:app_the_clark/presentation/pages/todays_deal.dart';
 import 'package:flutter/material.dart';
 
@@ -29,11 +31,12 @@ class AppMain extends StatefulWidget {
 }
 
 class _AppMainState extends State<AppMain> {
-  int _curentIndex = 0;
+  int _currentIndex = 0;
   final List<Widget> _widgetOption = <Widget>[
     const Home(),
     const PurchaseList(),
     const TodaysDeal(),
+    const FollowPage(),
     const MyPage(),
   ];
 
@@ -52,21 +55,19 @@ class _AppMainState extends State<AppMain> {
             type: BottomNavigationBarType.fixed,
             items: const [
               BottomNavigationBarItem(icon: Icon(Icons.home),label: "home"),
-              BottomNavigationBarItem(icon: Icon(Icons.leave_bags_at_home),label: "home"),
-              BottomNavigationBarItem(icon: Icon(Icons.percent),label: "home"),
-              BottomNavigationBarItem(icon: Icon(Icons.person),label: "home"),
+              BottomNavigationBarItem(icon: Icon(Icons.leave_bags_at_home),label: "reservation"),
+              BottomNavigationBarItem(icon: Icon(Icons.percent),label: "coupon"),
+              BottomNavigationBarItem(icon: Icon(Icons.phonelink_ring),label: "myPage"),
+              BottomNavigationBarItem(icon: Icon(Icons.person),label: "myPage"),
             ],
-            currentIndex: _curentIndex,
+            currentIndex: _currentIndex,
             onTap: (int index) {
               setState(() {
-                _curentIndex = index;
+                _currentIndex = index;
               });
             },
           ),
-          body: IndexedStack(
-            index: _curentIndex,
-            children: _widgetOption,
-          )
+          body: _widgetOption.elementAt(_currentIndex)
     ));
   }
 }
